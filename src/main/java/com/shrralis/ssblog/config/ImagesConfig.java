f4292c896aed277934b472faa921c4516ba18384
shrralis <yaroslavzhyravov@gmail.com>
2017-12-05 16:52:22 +0200

    - Added comment to ImagesConfig.

--- src/main/java/com/shrralis/ssblog/config/ImagesConfig.java
+++ src/main/java/com/shrralis/ssblog/config/ImagesConfig.java
@@ -2,7 +2,8 @@ package com.shrralis.ssblog.config;
 
 public class ImagesConfig {
     /**
-     * It will be something like '...tomcat/webapps/' + @link IMAGES_ROOT_PATH
+     * It will be something like '...tomcat/webapps/' +
+     * @see com.shrralis.ssblog.config.ImagesConfig#IMAGES_ROOT_PATH
      */
     public static final String IMAGES_ROOT_PATH = "/uploaded_images";
     public static final long IMAGES_MAX_SIZE = 3145728L;                            // it is 3 MB (1024 * 1024 * 3)
